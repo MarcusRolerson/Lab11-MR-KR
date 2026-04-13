73dd6fdd67d29fa026f1fb4fbcea8e940be58fbd
MarcusRolerson <marcus.a.rolerson@gmail.com>
2026-04-13 13:20:29 -0400

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -21,21 +21,20 @@ class TestCalculator(unittest.TestCase):
 
     ######## Partner 1
     def test_multiply(self):
-        self.assertEqual(mul(3, 4), 12)
-        self.assertEqual(mul(-2, 5), -10)
-        self.assertEqual(mul(0, 7), 0)
+        self.assertEqual(multiply(-2, 5), -10)
+        self.assertEqual(multiply(0, 7), 0)
 
 
     def test_divide(self):
-        self.assertEqual(div(2, 10), 5)
-        self.assertEqual(div(-2, 10), -5)
-        self.assertAlmostEqual(div(4, 9), 2.25)
+        self.assertEqual(divide(2, 10), 5)
+        self.assertEqual(divide(-2, 10), -5)
+        self.assertAlmostEqual(divide(4, 9), 2.25)
     # ##########################
 
     ######## Partner 2
     def test_divide_by_zero(self):
       with self.assertRaises(ZeroDivisionError):
-            div(0, 5)
+            divide(0, 5)
 
     def test_logarithm(self):
 
